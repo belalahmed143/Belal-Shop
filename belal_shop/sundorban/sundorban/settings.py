25ecc5d3fb35815d2f3644863514a3d46fafe3fc
belalahmed143 <mohammadbelalahmed82@gmail.com>
2022-03-22 14:44:30 +0600

    first commit

--- belal_shop/sundorban/sundorban/settings.py
+++ belal_shop/sundorban/sundorban/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0hl@-zq6o6jh%u288hc2=l^qwc4j!e3mb)7588h-&fh*)or1ca
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' , 'belalshop.herokuapp.com']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 
